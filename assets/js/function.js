8bf353c51d23cc9d362bf9b67c19e271cbf23631
QuentinDll <quentin.delplace13@gmail.com>
2018-06-25 09:21:52 +0200

    creation function switch img on mouse hover

---
+++ assets/js/function.js
@@ -0,0 +1,7 @@
+function change(){
+  document.getElementById('image1').src = 'assets/images/image1_2.jpg';
+  document.getElementById('image2').src = 'assets/images/image2_2.jpg';
+  document.getElementById('image3').src = 'assets/images/image3_2.jpg';
+  document.getElementById('image4').src = 'assets/images/image4_2.jpg';
+  document.getElementById('image5').src = 'assets/images/image5_2.jpg';
+}
